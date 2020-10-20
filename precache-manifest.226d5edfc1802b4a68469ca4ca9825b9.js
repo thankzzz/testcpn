5fe880523be8d1b42834e7c6f46ac0c8184869c1
thankzzz <ninoxfirst@gmail.com>
2020-10-20 10:52:56 +0700

    Updates

---
+++ precache-manifest.226d5edfc1802b4a68469ca4ca9825b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1fee1fe39fd9afb06a8f27632d64a2f",
+    "url": "/thankzzz/testcpn.git/index.html"
+  },
+  {
+    "revision": "87e6f04faa25e639a5c9",
+    "url": "/thankzzz/testcpn.git/static/css/main.f9140324.chunk.css"
+  },
+  {
+    "revision": "a746738a32dd45b3261e",
+    "url": "/thankzzz/testcpn.git/static/js/2.d2a5b48f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/thankzzz/testcpn.git/static/js/2.d2a5b48f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87e6f04faa25e639a5c9",
+    "url": "/thankzzz/testcpn.git/static/js/main.38440705.chunk.js"
+  },
+  {
+    "revision": "fb2552c690fca730e600",
+    "url": "/thankzzz/testcpn.git/static/js/runtime-main.21663a82.js"
+  }
+]);
